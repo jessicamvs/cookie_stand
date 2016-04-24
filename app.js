f0156302371a3fd6c233092f88ba71eb6fe03244
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-23 19:26:09 -0700

    Everything works perfectly for the pikePlace object

--- app.js
+++ app.js
@@ -1,66 +1,42 @@
-var hours = ["10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm"];
+var hours = ["10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
 
-var PikePlace = {
+var pikePlace = {
   name: "Pike Place",
   min: 17,
   max: 88,
   average: 5.2,
-  total: 0,
+  totalDemand: 0,
   demands: [],
   //This genereates a random number of customers between min and max
   generateRandom: function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   },
-  //This calculates
-  calculate: function() {
+  //This calculates cookie demand each hour
+  hourlyDemand: function() {
     for(hour in hours) {
-      var cookiesThisHour = Math.floor(this.generateRandom(this.min, this.max) * this.average);
-      this.demands.push(cookiesThisHour);
-      this.total += cookiesThisHour;
+      this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
+      this.totalDemand += this.demands[hour];
     }
   },
 //This prints the lists to the page
   render: function() {
-    this.calculate();
-    var sectionEl = document.getElementById("stores");
+    this.hourlyDemand();
+    var sectionEl = document.getElementById("pikePlaceSales");
+    sectionEl.textContent = this.name;
     var ulEl = document.createElement("ul");
-
+//This creates li for each hour and appends it to the ul we created above
     for(hour in hours) {
       var liEl = document.createElement("li");
       liEl.textContent = hours[hour] + ": " + this.demands[hour];
-      var nestedUlEl = document.createElement("ul");
-
-      liEl.appendChild(nestedUlEl);
       ulEl.appendChild(liEl);
     }
+//This creates and li for totalDemand and appends it to the ul created above
+//then appends it to the existing section within our HTML.
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalDemand;
+    ulEl.appendChild(liEl);
     sectionEl.appendChild(ulEl);
   }
 }
 
-PikePlace.render();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //
-  //
-  //
-  //
-  //
-  // total: function() {
-  //   for(var = j; j < 8; j++) {
-  //     return total += calculate();
-  //   }
-  // },
+pikePlace.render();
