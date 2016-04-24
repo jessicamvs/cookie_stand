02541accfb087518be0148203a754f719979d572
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-23 19:36:46 -0700

    All stores are now printing to the page

--- app.js
+++ app.js
@@ -163,3 +163,44 @@ var bellevueSquare = {
 }
 
 bellevueSquare.render();
+
+var alki = {
+  name: "Alki",
+  min: 3,
+  max: 24,
+  average: 2.6,
+  totalDemand: 0,
+  demands: [],
+  //This genereates a random number of customers between min and max
+  generateRandom: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  //This calculates cookie demand each hour
+  hourlyDemand: function() {
+    for(hour in hours) {
+      this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
+      this.totalDemand += this.demands[hour];
+    }
+  },
+//This prints the lists to the page
+  render: function() {
+    this.hourlyDemand();
+    var sectionEl = document.getElementById("alkiSales");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");
+//This creates li for each hour and appends it to the ul we created above
+    for(hour in hours) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hours[hour] + ": " + this.demands[hour];
+      ulEl.appendChild(liEl);
+    }
+//This creates and li for totalDemand and appends it to the ul created above
+//then appends it to the existing section within our HTML.
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalDemand;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+
+alki.render();
