75524c041a6896b236e0212eaf4ba60883f45c05
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-23 19:33:53 -0700

    Four stores are now printing to the page

--- app.js
+++ app.js
@@ -40,3 +40,126 @@ var pikePlace = {
 }
 
 pikePlace.render();
+
+var seaTacAirport = {
+  name: "SeaTac Aiport",
+  min: 6,
+  max: 24,
+  average: 1.2,
+  totalDemand: 0,
+  demands: [],
+  //This genereates a random number of customers between min and max
+  generateRandom: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  //This calculates cookie demand each hour
+  hourlyDemand: function() {
+    for(hour in hours) {
+      this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
+      this.totalDemand += this.demands[hour];
+    }
+  },
+//This prints the lists to the page
+  render: function() {
+    this.hourlyDemand();
+    var sectionEl = document.getElementById("seaTacAirportSales");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");
+//This creates li for each hour and appends it to the ul we created above
+    for(hour in hours) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hours[hour] + ": " + this.demands[hour];
+      ulEl.appendChild(liEl);
+    }
+//This creates and li for totalDemand and appends it to the ul created above
+//then appends it to the existing section within our HTML.
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalDemand;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+
+seaTacAirport.render();
+
+var southcenter = {
+  name: "Southcenter",
+  min: 11,
+  max: 38,
+  average: 1.9,
+  totalDemand: 0,
+  demands: [],
+  //This genereates a random number of customers between min and max
+  generateRandom: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  //This calculates cookie demand each hour
+  hourlyDemand: function() {
+    for(hour in hours) {
+      this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
+      this.totalDemand += this.demands[hour];
+    }
+  },
+//This prints the lists to the page
+  render: function() {
+    this.hourlyDemand();
+    var sectionEl = document.getElementById("southcenterSales");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");
+//This creates li for each hour and appends it to the ul we created above
+    for(hour in hours) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hours[hour] + ": " + this.demands[hour];
+      ulEl.appendChild(liEl);
+    }
+//This creates and li for totalDemand and appends it to the ul created above
+//then appends it to the existing section within our HTML.
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalDemand;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+
+southcenter.render();
+
+var bellevueSquare = {
+  name: "Bellevue Square",
+  min: 20,
+  max: 48,
+  average: 3.3,
+  totalDemand: 0,
+  demands: [],
+  //This genereates a random number of customers between min and max
+  generateRandom: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  //This calculates cookie demand each hour
+  hourlyDemand: function() {
+    for(hour in hours) {
+      this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
+      this.totalDemand += this.demands[hour];
+    }
+  },
+//This prints the lists to the page
+  render: function() {
+    this.hourlyDemand();
+    var sectionEl = document.getElementById("bellevueSquareSales");
+    sectionEl.textContent = this.name;
+    var ulEl = document.createElement("ul");
+//This creates li for each hour and appends it to the ul we created above
+    for(hour in hours) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hours[hour] + ": " + this.demands[hour];
+      ulEl.appendChild(liEl);
+    }
+//This creates and li for totalDemand and appends it to the ul created above
+//then appends it to the existing section within our HTML.
+    liEl = document.createElement("li");
+    liEl.textContent = "Total: " + this.totalDemand;
+    ulEl.appendChild(liEl);
+    sectionEl.appendChild(ulEl);
+  }
+}
+
+bellevueSquare.render();
