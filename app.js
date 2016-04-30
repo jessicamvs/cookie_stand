b6b515003ce1ab3a517a8f5c0a44ee8193a45470
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-30 11:50:04 -0700

    Three stores are now printing to the page

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ Store.prototype.hourlyDemand = function() {
 
 Store.prototype.render = function() {
   this.hourlyDemand();
-  var sectionEl = document.getElementById('pikePlaceSales');
-  sectionEl.textContent = this.storeName;
+  var sectionEl = document.getElementById('stores');
+  var newStoreSection = document.createElement('section');
+  newStoreSection.textContent = this.storeName;
+  sectionEl.appendChild(newStoreSection);
   var ulEl = document.createElement('ul');
 //This creates li for each hour and appends it to the ul we created above
   for(hour in hours) {
@@ -39,48 +41,12 @@ Store.prototype.render = function() {
 };
 
 var pikePlace = new Store('Pike Place', 17, 88, 5.2);
-pikePlace.render();
+var seaTacAirport = new Store('SeaTac Airport', 6, 24, 1.2);
+var southcenter = new Store('Souchcenter', 11, 38, 1.9);
 
-// var seaTacAirport = {
-//   name: 'SeaTac Aiport',
-//   min: 6,
-//   max: 24,
-//   average: 1.2,
-//   totalDemand: 0,
-//   demands: [],
-//   //This genereates a random number of customers between min and max
-//   generateRandom: function(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   },
-//   //This calculates cookie demand each hour
-//   hourlyDemand: function() {
-//     for(hour in hours) {
-//       this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
-//       this.totalDemand += this.demands[hour];
-//     }
-//   },
-// //This prints the lists to the page
-//   render: function() {
-//     this.hourlyDemand();
-//     var sectionEl = document.getElementById('seaTacAirportSales');
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement('ul');
-// //This creates li for each hour and appends it to the ul we created above
-//     for(hour in hours) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[hour] + ': ' + this.demands[hour];
-//       ulEl.appendChild(liEl);
-//     }
-// //This creates and li for totalDemand and appends it to the ul created above
-// //then appends it to the existing section within our HTML.
-//     liEl = document.createElement('li');
-//     liEl.textContent = 'Total: ' + this.totalDemand;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// };
-//
-// seaTacAirport.render();
+pikePlace.render();
+seaTacAirport.render();
+southcenter.render();
 //
 // var southcenter = {
 //   name: 'Southcenter',
