f14e3b144943a5d32aaf1327e9e10bab5bacc9a9
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-30 11:56:59 -0700

    All stores are now printing to the page

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ Store.prototype.generateRandom = function() {
 
 Store.prototype.hourlyDemand = function() {
   for(hour in hours) {
-    this.demands.push(Math.floor(this.generateRandom(this.minCust, this.maxCust) * this.avgCookie));      this.totalDemand += this.demands[hour];
+    this.demands.push(Math.floor(this.generateRandom() * this.avgCookie));
+    this.totalDemand += this.demands[hour];
   }
 };
 
@@ -43,128 +44,11 @@ Store.prototype.render = function() {
 var pikePlace = new Store('Pike Place', 17, 88, 5.2);
 var seaTacAirport = new Store('SeaTac Airport', 6, 24, 1.2);
 var southcenter = new Store('Souchcenter', 11, 38, 1.9);
+var bellevueSquare = new Store('Bellevue Square', 20, 48, 3.3);
+var alki = new Store('Alki', 3, 24, 2.6);
 
 pikePlace.render();
 seaTacAirport.render();
 southcenter.render();
-//
-// var southcenter = {
-//   name: 'Southcenter',
-//   min: 11,
-//   max: 38,
-//   average: 1.9,
-//   totalDemand: 0,
-//   demands: [],
-//   //This genereates a random number of customers between min and max
-//   generateRandom: function(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   },
-//   //This calculates cookie demand each hour
-//   hourlyDemand: function() {
-//     for(hour in hours) {
-//       this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
-//       this.totalDemand += this.demands[hour];
-//     }
-//   },
-// //This prints the lists to the page
-//   render: function() {
-//     this.hourlyDemand();
-//     var sectionEl = document.getElementById('southcenterSales');
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement('ul');
-// //This creates li for each hour and appends it to the ul we created above
-//     for(hour in hours) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[hour] + ': ' + this.demands[hour];
-//       ulEl.appendChild(liEl);
-//     }
-// //This creates and li for totalDemand and appends it to the ul created above
-// //then appends it to the existing section within our HTML.
-//     liEl = document.createElement('li');
-//     liEl.textContent = 'Total: ' + this.totalDemand;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// };
-//
-// southcenter.render();
-//
-// var bellevueSquare = {
-//   name: 'Bellevue Square',
-//   min: 20,
-//   max: 48,
-//   average: 3.3,
-//   totalDemand: 0,
-//   demands: [],
-//   //This genereates a random number of customers between min and max
-//   generateRandom: function(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   },
-//   //This calculates cookie demand each hour
-//   hourlyDemand: function() {
-//     for(hour in hours) {
-//       this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
-//       this.totalDemand += this.demands[hour];
-//     }
-//   },
-// //This prints the lists to the page
-//   render: function() {
-//     this.hourlyDemand();
-//     var sectionEl = document.getElementById('bellevueSquareSales');
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement('ul');
-// //This creates li for each hour and appends it to the ul we created above
-//     for(hour in hours) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[hour] + ': ' + this.demands[hour];
-//       ulEl.appendChild(liEl);
-//     }
-// //This creates and li for totalDemand and appends it to the ul created above
-// //then appends it to the existing section within our HTML.
-//     liEl = document.createElement('li');
-//     liEl.textContent = 'Total: ' + this.totalDemand;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// };
-//
-// bellevueSquare.render();
-//
-// var alki = {
-//   name: 'Alki',
-//   min: 3,
-//   max: 24,
-//   average: 2.6,
-//   totalDemand: 0,
-//   demands: [],
-//   //This genereates a random number of customers between min and max
-//   generateRandom: function(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   },
-//   //This calculates cookie demand each hour
-//   hourlyDemand: function() {
-//     for(hour in hours) {
-//       this.demands.push(Math.floor(this.generateRandom(this.min, this.max) * this.average));
-//       this.totalDemand += this.demands[hour];
-//     }
-//   },
-// //This prints the lists to the page
-//   render: function() {
-//     this.hourlyDemand();
-//     var sectionEl = document.getElementById('alkiSales');
-//     sectionEl.textContent = this.name;
-//     var ulEl = document.createElement('ul');
-// //This creates li for each hour and appends it to the ul we created above
-//     for(hour in hours) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[hour] + ': ' + this.demands[hour];
-//       ulEl.appendChild(liEl);
-//     }
-// //This creates and li for totalDemand and appends it to the ul created above
-// //then appends it to the existing section within our HTML.
-//     liEl = document.createElement('li');
-//     liEl.textContent = 'Total: ' + this.totalDemand;
-//     ulEl.appendChild(liEl);
-//     sectionEl.appendChild(ulEl);
-//   }
-// };
+bellevueSquare.render();
+alki.render();
